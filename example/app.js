cb89b3fcf800f4226d4641b3044c771d68eb8439
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-06-15 18:03:41 +0200

    Update app.js

--- example/app.js
+++ example/app.js
@@ -1,22 +1,15 @@
-// This is a test harness for your module
-// You should do something interesting in this harness
-// to test out the module and to provide instructions
-// to users on how to use it by example.
+var io = require('ti.socketio');
+var socket = io.connect('http://localhost:8080/', { /* Options */ });
 
-
-// open a single window
 var win = Ti.UI.createWindow({
-	backgroundColor: 'white'
+  backgroundColor: 'white'
 });
-var label = Ti.UI.createLabel({text: 'SocketIO'});
+
+var label = Ti.UI.createLabel({ text: 'Connecting ...' });
 win.add(label);
 win.open();
 
-var io = require('ti.socketio');
-var socket = io.connect("http://localhost:8080/");
-label.text = 'Connecting ...';
 socket.on('connect', function() {
-	label.text = 'Socket connected';
-
-	socket.emit('hello', 'world');
+  label.text = 'Socket connected';
+  socket.emit('hello', 'world');
 });
