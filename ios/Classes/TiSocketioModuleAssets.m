7d5752e172f893fd0371f88080bc0bc62371384a
Hans Knöchel <h.knoechel@lambus.com>
2022-05-15 10:21:31 +0200

    fix: fomat

--- ios/Classes/TiSocketioModuleAssets.m
+++ ios/Classes/TiSocketioModuleAssets.m
@@ -3,20 +3,18 @@
  */
 #import "TiSocketioModuleAssets.h"
 
-extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+extern NSData *filterDataInRange(NSData *thedata, NSRange range);
 
 @implementation TiSocketioModuleAssets
 
 - (NSData *)moduleAsset
 {
-  
 
   return nil;
 }
 
 - (NSData *)resolveModuleAsset:(NSString *)path
 {
-  
 
   return nil;
 }
